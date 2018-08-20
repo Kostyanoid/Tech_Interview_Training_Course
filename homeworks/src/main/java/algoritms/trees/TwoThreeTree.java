6b9260e4a62efb04b0e8c536fed43cd2d9d89c02
Konstantin_Romanov <romanov.konstantin.il@gmail.com>
2018-08-21 01:09:38 +0300

    2-3 tree implementation in progress

--- homeworks/src/main/java/algoritms/trees/TwoThreeTree.java
+++ homeworks/src/main/java/algoritms/trees/TwoThreeTree.java
@@ -11,7 +11,7 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
 
     private class Entry<K, V> {
         private final K key;
-        private final V value;
+        private V value;
 
         public Entry(K key, V value) {
             this.key = key;
@@ -26,6 +26,10 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
             return value;
         }
 
+        public void setValue(V value) {
+            this.value = value;
+        }
+
         @Override
         public String toString() {
             final StringBuilder sb = new StringBuilder("[");
@@ -35,7 +39,7 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
             return sb.toString();
         }
     }
-    private class Node<K, V> {
+    private class Node<K extends Comparable<K>, V> {
         private final List<Entry<K, V>> entries;
         private Node<K, V> top;
         private Node<K, V> left;
@@ -51,24 +55,6 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
         public Node(K key, V value) {
             this();
             insertEntry(key, value);
-        }
-
-        public List<K> getKeys() {
-            return entries.stream().map(Entry::getKey).collect(Collectors.toList());
-        }
-
-
-        public List<V> getValues() {
-            return entries.stream().map(Entry::getValue).collect(Collectors.toList());
-        }
-
-        public int containsKey(K key) {
-            return getKeys().indexOf(key);
-        }
-
-        public void insertEntry(K key, V value) {
-            if (key == null) throw new IllegalStateException("Key can not be null!");
-            entries.add(new Entry<>(key, value));
         }
 
         public Node<K, V> getTop() {
@@ -103,16 +89,59 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
             this.mid = mid;
         }
 
-        public boolean isLeaf() {
-            return left == null && right == null && mid == null;
-        }
-
         public int getCapacity() {
             return capacity;
         }
 
         public void setCapacity(int capacity) {
             this.capacity = capacity;
+        }
+
+        public List<K> getKeys() {
+            return entries.stream().map(Entry::getKey).collect(Collectors.toList());
+        }
+
+        public List<V> getValues() {
+            return entries.stream().map(Entry::getValue).collect(Collectors.toList());
+        }
+
+        public int containsKey(K key) {
+            return getKeys().indexOf(key);
+        }
+
+        public  int keysCount() {
+            return getKeys().size();
+        }
+
+        public void insertEntry(K key, V value) {
+            if (key == null) throw new IllegalStateException("Key can not be null!");
+            entries.add(new Entry<>(key, value));
+            capacity++;
+        }
+
+        private void sortKeys() {
+            if (keysCount() <= 1) return;
+            if (keysCount() == 2) {
+                //TODO: sort
+            }
+        }
+
+        private void swapEntries(int i1, int i2) {
+            assert (i1 >=0 && i1 < entries.size() && i2 >=0 && i2 < entries.size());
+            if (i1 == i2) return;
+            Entry<K, V> temp = entries.get(i1);
+            entries.set(i1, entries.get(i2));
+            entries.set(i2, temp);
+        }
+
+        public boolean isLeaf() {
+            return left == null && right == null && mid == null;
+        }
+
+        public void changeValueByKey(K key, V value) {
+            int i = containsKey(key);
+            if (i < 0 ) return;
+            entries.get(i).setValue(value);
         }
 
     }
@@ -123,15 +152,37 @@ public class TwoThreeTree<K extends Comparable<K>, V> implements ITree<K, V> {
             root = new Node<>(key, value);
             size = 1;
         }
-        //TODO: insert
+        Node<K, V> n = seekNodeForInsertKey(root, key);
+
+        if (n.getKeys().contains(key)) {
+            n.changeValueByKey(key, value);
+            return;
+        }
+
+        if (!n.isLeaf() && n.keysCount() == 2) {
+            Node<K, V> newNode = new Node<>(key, value);
+            newNode.setTop(n);
+            n.setMid(newNode);
+        } else {
+            n.insertEntry(key, value);
+            if (n.keysCount() == 3) {
+                dividingNode(n);
+            }
+        }
+        size++;
     }
 
-    private Node<K, V> seekNodForInsertKey(Node<K, V> top, K key) {
+    private Node<K, V> seekNodeForInsertKey(Node<K, V> top, K key) {
         if (top.isLeaf() || top.getKeys().contains(key)) return top;
-        if (top.getKeys().get(0).compareTo(key) < 0 ) seekNodForInsertKey(top.getLeft(), key);
-        else if (top.getKeys().get(top.getKeys().size() - 1).compareTo(key) > 0) seekNodForInsertKey(top.getRight(), key);
-        else if (top.getMid() != null) seekNodForInsertKey(top.getMid(), key);
+        if (top.getKeys().get(0).compareTo(key) > 0 ) seekNodeForInsertKey(top.getLeft(), key);
+        else if (top.getKeys().get(top.getKeys().size() - 1).compareTo(key) < 0) seekNodeForInsertKey(top.getRight(), key);
+        else if (top.getMid() != null) seekNodeForInsertKey(top.getMid(), key);
         return top;
+    }
+
+    private void dividingNode(Node<K, V> node) {
+        if (node.keysCount() < 3) return;
+
     }
 
     @Override
